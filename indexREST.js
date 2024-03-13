98b8dd0c6c0646aeb61f708ec6575ba5dccc6910
Bhandari Mahesh <s2300094@BC-C02GR0BSQ6LRs-MacBook-Air.local>
2024-03-13 02:45:19 +0200

    Created absolute json file path

--- indexREST.js
+++ indexREST.js
@@ -10,10 +10,10 @@ const dataAccessLayerPath = process.env.DATA_ACCESS_LAYER_PATH ||
 path.join(__dirname, 'data-access-layer', 'data_access_layer.js');
 
 // Data and DataAccessLayer from data_access_layer.js
-const { DataAccessLayer } = require(dataAccessLayerPath);
+const { DataAccessLayer } = require('./data-access-layer/data_access_layer.js');
 
 // Construct the file path using the environment variable or retrieve the JSON file path from the environment variable
-const jsonFilePath = process.env.JSON_FILE_PATH || path.join(__dirname, 'bhandari_mahesh_books.json');
+const jsonFilePath = path.join(__dirname, 'bhandari_mahesh_books.json');
 
 // Create an instance of DataAccessLayer with the JSON file path from the environment variable
 const dataAccessLayer = new DataAccessLayer(jsonFilePath)
